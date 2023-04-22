2f06c8699a72628d4bb054a4d343ea6305e1913a
EdgarLuna <eluna@urchinsys.com>
2023-04-21 19:57:11 -0500

    implementando/modificando creación de usuarios

--- api/routers/session_router.py
+++ api/routers/session_router.py
@@ -2,14 +2,19 @@ import schemas
 import common
 import adapters
 
-from fastapi import APIRouter, Depends, Response
+from fastapi import APIRouter, Depends, Response, Request
 
 session_router = APIRouter(prefix="/session", tags=["mock resource"])
 
 
 @session_router.post("/create_user")
-def create_user(new_user_schema: schemas.CreateUserSchema, token: str = Depends(common.token_schema)):
-    pass
+def create_user(new_user_schema: schemas.CreateUserSchema, request: Request, response: Response,
+                token: str = Depends(common.token_schema)):
+    adapter = adapters.AuthAdapter()
+    headers = dict(request.headers.items())
+    status_code, json_response = adapter.create_user(new_user_schema, headers)
+    response.status_code = status_code
+    return json_response
 
 
 @session_router.post("/login")
--- helpers/hash.py
+++ helpers/hash.py
@@ -1,10 +1,25 @@
 import hashlib
 import json
+import typing
+
+from cryptography.fernet import Fernet
+
+from config import AppConfigValues
 
 
-def get_hash(payload):
+def get_hash(payload: typing.Dict):
+    payload = dict(filter(lambda kv: kv[0] not in ["hash"], payload.items()))
     return hashlib.md5(json.dumps(payload, separators=(",", ":"),
                                   sort_keys=True, ensure_ascii=False).encode()).hexdigest()
 
 
-__all__ = ["get_hash"]
+def encrypt_hash(hash_sum: typing.Union[str, bytes]) -> str:
+    fernet = Fernet(AppConfigValues.ENCRYPTION_KEY_SECRET.encode())
+    return fernet.encrypt(hash_sum.encode() if isinstance(hash_sum, str) else hash_sum).decode()
+
+
+def refresh_object_hash(obj: dict):
+    obj["hash"] = encrypt_hash(get_hash(obj))
+
+
+__all__ = ["get_hash", "encrypt_hash", "refresh_object_hash"]
