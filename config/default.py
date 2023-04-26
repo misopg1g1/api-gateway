36207af7e11d1abd98974f3dc6fff0f7aa06ff34
EdgarLuna <eluna@urchinsys.com>
2023-04-25 19:26:25 -0500

    modificando verify_role middleware y ruta del metodo put_inventory en el adaptador de inventarios

--- config/default.py
+++ config/default.py
@@ -5,5 +5,5 @@ class AppConfigValues:
     ENCRYPTION_KEY_SECRET = os.getenv("ENCRYPTION_KEY_SECRET", "ASuJ-vtjlvAuUdFDTdeMOHoCTjlS_dipLtp6_7rQ_kw=")
     AUTH_URL = os.getenv("AUTH_URL", "http://localhost:3001")
     PRODUCTS_URL = os.getenv("PRODUCTS_URL", "http://localhost:3002")
-    INVENTORY_URL = os.getenv("INVENTORY_URL", "http://localhost:3001")
+    INVENTORY_URL = os.getenv("INVENTORY_URL", "http://localhost:3002")
 
