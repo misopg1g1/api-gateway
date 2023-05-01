68e9a933000cbe323e0d31d7f9d792f46106cdc9
EdgarLuna <eluna@urchinsys.com>
2023-04-30 22:12:24 -0500

    arreglando problema de compilacion

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -120,12 +120,12 @@ class CreateProductSchema(pydantic.BaseModel):
     dimensions: typing.Optional[str] = pydantic.Field(default="")
     type: ProductType = pydantic.Field(...)
     temperature_control: typing.Optional[int] = pydantic.Field(default="")
-    expiration_date: typing.Optional[str] = pydantic.Field(...)
+    expiration_date: typing.Optional[str] = None
     fragility_conditions: typing.Optional[str] = pydantic.Field(default="")
     description: typing.Optional[str] = pydantic.Field(default="")
     status: typing.Optional[bool] = pydantic.Field(default=True)
     price: int = pydantic.Field(...)
-    img_base64_data: typing.Optional[str] = pydantic.Field(...)
+    img_base64_data: typing.Optional[str] = None
     suppliers: typing.Optional[typing.List[str]] = pydantic.Field(default=[])
     categories: typing.List[str] = pydantic.Field(...)
 
