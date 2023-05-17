a84634d7c3c026ffad642d2a767fb8c801f1b888
EdgarLuna <eluna@urchinsys.com>
2023-05-17 15:54:46 -0500

    eliminando ruta de prueba del health router

--- api/routers/health_router.py
+++ api/routers/health_router.py
@@ -9,11 +9,3 @@ health_router = APIRouter(prefix="/health", tags=["Health Check"])
 def health():
     '''retorna 200 si el microservicio esta activo.'''
     return {"msg": "healthy!!!!!"}
-
-
-@health_router.post("/kombu")
-def health(data=Body()):
-    publisher = publishers.new_order_email_publisher()
-    publisher.publish_user_to_verify(data)
-    publisher.connection.close()
-    return {"msg": "mensaje enviado a la cola!!"}
